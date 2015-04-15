5ad99fa3564f99b844cd759ea091691002c5513e
whicks <m.weshicks@gmail.com>
2015-04-14 21:58:44 -0400

    Add content main style, blog css classes

--- app/view/blog.php
+++ app/view/blog.php
@@ -16,12 +16,13 @@ $body = $blog_gui->show_all();
 <div class="container">
 	<div class="row">
 		<div class="col-md-12">
-			
-			<h2>Blog goes here</h2>
+			<div class="content-main">
+                <h2>Blog goes here</h2>
 
-            <?php
+                <?php
                 echo $body;
-            ?>
+                ?>
+            </div>
 		</div>
 	</div>
 </div>
