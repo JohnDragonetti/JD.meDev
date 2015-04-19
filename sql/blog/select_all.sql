5f29551c73504f244bb389726e87f9d647b9818e
whicks <m.weshicks@gmail.com>
2015-04-19 16:50:23 -0400

    Add SQL for selecting all blog posts.

---
+++ sql/blog/select_all.sql
@@ -0,0 +1,9 @@
+SELECT
+    `blog_post`.`post_id`,
+    `blog_post`.`title`,
+    `blog_post`.`body`,
+    `blog_post`.`category`,
+    `blog_post`.`created`,
+    `blog_post`.`created_by`
+FROM
+    `blog_post`;
