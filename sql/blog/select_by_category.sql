c85018eab49c47a75c05be9136bf7b5ff0c42972
whicks <m.weshicks@gmail.com>
2015-04-19 16:54:38 -0400

    Add select by category id SQL

---
+++ sql/blog/select_by_category.sql
@@ -0,0 +1,11 @@
+SELECT
+  `blog_post`.`post_id`,
+  `blog_post`.`title`,
+  `blog_post`.`body`,
+  `blog_post`.`category`,
+  `blog_post`.`created`,
+  `blog_post`.`created_by`
+FROM
+  `blog_post`
+WHERE
+  `blog_post`.`category_id` = ?
